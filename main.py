af91e14fdf1b825f14742d0c377650e7e3d7800f
pontakornth <pontakorn61@gmail.com>
2021-03-29 21:15:22 +0700

    feat: make basic calculation work

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
+from enum import Enum
 from fastapi import FastAPI
+from typing import Optional
+import pydantic
+import math
+
+class AvailableFunction(str, Enum):
+    sin = 'sin'
+    cos = 'cos'
+    tan = 'tan'
+
+class Calculation(pydantic.BaseModel):
+    result: float
 
 app = FastAPI()
 
 @app.get('/')
-async def kono_dio_da():
+async def welcome_message():
     return {'message': 'Kono DIO da'}
 
-@app.get('/complex')
-async def complex_dio_da():
-    response = {i: i+1 for i in range(12)}
-    return response
+# Warning: This only for testing purpose only. Do not do this on production
+@app.get('/calculate/{function}')
+async def calculate(function: AvailableFunction, parameter: Optional[float]=0):
+    if function == AvailableFunction.sin:
+        return Calculation(result=math.sin(parameter))
+    elif function == AvailableFunction.cos:
+        return Calculation(result=math.cos(parameter))
+    elif function == AvailableFunction.tan:
+        return Calculation(result=math.tan(parameter))
